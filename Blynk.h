3b76a0fcba73256c1f19c9aa3808e375c0fae88c
James Griffin <JamesTM5@users.noreply.github.com>
2016-11-20 22:17:01 +0000

    Basic Blynk connection with debug statement to ensure it works.

---
+++ Blynk.h
@@ -0,0 +1,30 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define BLYNK_PRINT Serial 
+#define server "blynk.cloud-com"
+//#define server "192.168.0.12"
+char auth[] = "86dc4118de9842ba99b611b61c1c2276";
+char ssid[] = "virginmedia6125068";
+char pass[] = "jnaascuw";
+
+
+void reconnectBlynk() {
+  Serial.print("Checking connection");
+  if (!Blynk.connected()) {
+    if(Blynk.connect()) {
+      BLYNK_LOG("Reconnected");
+    } else {
+      BLYNK_LOG("Not reconnected");
+    }
+  }
+}
+
+void serverConnect() {
+  int mytimeout = millis() / 1000;
+  while (Blynk.connect() == false) { // try to connect to server for 10 seconds
+    if((millis() / 1000) > mytimeout + 8){ // try local server if not connected within 9 seconds
+       break;
+    }
+  }
+}
---
+++ Testing_rebuild.ino
@@ -0,0 +1,25 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include "Blynk.h"
+#include <SimpleTimer.h>
+#include <TimeLib.h>
+
+
+SimpleTimer timer;
+
+void setup() {
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass, server);
+  serverConnect();
+
+  timer.setInterval(30000L, reconnectBlynk);  // check every 30s if still connected to server 
+  }
+
+void loop() {
+  if (Blynk.connected()) {
+    Blynk.run();
+  }
+
+  timer.run();
+};
